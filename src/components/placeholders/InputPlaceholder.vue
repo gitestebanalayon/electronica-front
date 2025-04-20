041f82f4734e4d0203a8077137a6cd5021f14110
gitestebanalayon <gitesteban0@gmail.com>
2025-04-19 23:40:31 -0400

    dsad

--- src/components/placeholders/InputPlaceholder.vue
+++ src/components/placeholders/InputPlaceholder.vue
@@ -5,3 +5,11 @@
 <template>
     <div class="placeholder" style="height: 39.6px; width: 100%;"></div>
 </template>
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="placeholder" style="height: 39.6px; width: 100%;"></div>
+</template>
